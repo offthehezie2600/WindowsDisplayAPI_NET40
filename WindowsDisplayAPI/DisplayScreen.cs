dfdd0d4bbac2558124fb5aa8eec0cedd64a5e0a8
offthehezie2600 <5864841+offthehezie2600@users.noreply.github.com>
2025-09-14 23:03:36 -0500

    Change target to net40.  Update references for .net40 windows forms

--- WindowsDisplayAPI/DisplayScreen.cs
+++ WindowsDisplayAPI/DisplayScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Windows.Forms;
 using System.Linq;
 using WindowsDisplayAPI.DisplayConfig;
 using WindowsDisplayAPI.Exceptions;
